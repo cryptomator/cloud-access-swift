2831e38d0a6be2454dea5bca7eb73e971e51c2d8
Tobias Hagemann <tobias.hagemann@skymatic.de>
2020-06-12 19:12:00 +0200

    Refactored some CloudProvider convenience methods

--- CloudAccess/Core/CloudProvider+Convenience.swift
+++ CloudAccess/Core/CloudProvider+Convenience.swift
@@ -40,15 +40,14 @@ public extension CloudProvider {
 	}
 
 	/**
-	 Convenience Wrapper for `deleteItem()` , which satisfies also if the item is not present.
+	 Convenience wrapper for `deleteItem()` that also satisfies if the item is not present.
 	 */
 	func deleteItemIfExists(at remoteURL: URL) -> Promise<Void> {
-		return Promise(on: .global()) { fulfill, _ in
-			do {
-				try await(self.deleteItem(at: remoteURL))
-				fulfill(())
-			} catch CloudProviderError.itemNotFound {
-				fulfill(())
+		return deleteItem(at: remoteURL).recover { error -> Promise<Void> in
+			if case CloudProviderError.itemNotFound = error {
+				return Promise(())
+			} else {
+				return Promise(error)
 			}
 		}
 	}
@@ -57,12 +56,13 @@ public extension CloudProvider {
 	 Checks if the item exists at the given remoteURL.
 	 */
 	func checkForItemExistence(at remoteURL: URL) -> Promise<Bool> {
-		return Promise(on: .global()) { fulfill, _ in
-			do {
-				_ = try await(self.fetchItemMetadata(at: remoteURL))
-				fulfill(true)
-			} catch CloudProviderError.itemNotFound {
-				fulfill(false)
+		return fetchItemMetadata(at: remoteURL).then { _ in
+			return Promise(true)
+		}.recover { error -> Promise<Bool> in
+			if case CloudProviderError.itemNotFound = error {
+				return Promise(false)
+			} else {
+				return Promise(error)
 			}
 		}
 	}

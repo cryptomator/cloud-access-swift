2865178ee28e222cf20d04e79164121b2907e6cd
Sebastian Stenzel <sebastian.stenzel@gmail.com>
2020-06-18 15:35:03 +0200

    updated api, added tests for first impl

--- CloudAccess/VaultFormat7/VaultFormat7ShortenedNameCache.swift
+++ CloudAccess/VaultFormat7/VaultFormat7ShortenedNameCache.swift
@@ -6,44 +6,112 @@
 //  Copyright © 2020 Skymatic GmbH. All rights reserved.
 //
 
+import CommonCrypto
 import Foundation
 import Promises
 
 struct ShortenedURL {
 	let url: URL
-	let isShortened: Bool
+	let originalName: String
+	let state: ShorteningState
 	let nameFileURL: URL?
+}
 
-	init(url: URL, isShortened: Bool, nameFileURL: URL?) {
-		self.url = url
-		self.isShortened = isShortened
-		self.nameFileURL = nameFileURL
+enum ShorteningState {
+	/**
+	 URL has not been shortened
+	 */
+	case unshortened
+
+	/**
+	 URL has been shortened in its last path component
+	 */
+	case shortenedChild
+
+	/**
+	 URL has been shortened, but not in its last path component
+	 */
+	case shortenedAncester
+}
+
+private extension URL {
+	func lastPathComponents(_ count: Int) -> [String] {
+		let comps = pathComponents
+		let n = comps.count
+		return Array(comps[n - count ... n - 1])
+	}
+
+	func deletingLastPathComponents(_ count: Int) -> URL {
+		precondition(count >= 0)
+		if count == 0 {
+			return self
+		} else {
+			return deletingLastPathComponents(count - 1).deletingLastPathComponent()
+		}
+	}
+
+	func appendingPathComponents(pathComponents: [String], isDirectory: Bool) -> URL {
+		if pathComponents.count == 1 {
+			return appendingPathComponent(pathComponents[0], isDirectory: isDirectory)
+		} else {
+			let remainingComponents = Array(pathComponents[1...])
+			return appendingPathComponent(pathComponents[0]).appendingPathComponents(pathComponents: remainingComponents, isDirectory: isDirectory)
+		}
+	}
+
+	func replacingPathComponent(atIndex index: Int, with replacement: String, isDirectory: Bool) -> URL {
+		precondition(index < pathComponents.count)
+		let tailSize = pathComponents.count - index
+		assert(tailSize > 0)
+		var tail = lastPathComponents(tailSize)
+		let prefix = deletingLastPathComponents(tailSize)
+		tail[0] = replacement
+		return prefix.appendingPathComponents(pathComponents: tail, isDirectory: isDirectory)
 	}
 }
 
-class VaultFormat7ShortenedNameCache {
+internal class VaultFormat7ShortenedNameCache {
+	static let threshold = 220
+
+	let vaultURL: URL
+	let ciphertextNameCompIdx: Int
+
+	init(vaultURL: URL) {
+		self.vaultURL = vaultURL
+		self.ciphertextNameCompIdx = vaultURL.pathComponents.endIndex - 1 + 4
+	}
 
 	/**
 	 Applies the .c9s shortening defined in vault format 7 (if required).
 	 This **does not** persist the original name to `name.c9s`, though.
 
-	- Parameter originalURL: The unshortened URL.
-	- Returns: A `ShortenedURL` object that is either based on the `originalURL` (if no shortening is required) or a shortened URL
-	*/
-	func getShortenedURL(_ originalURL: URL) -> ShortenedURL {
-		return ShortenedURL(url: originalURL, isShortened: false, nameFileURL: nil)
+	 - Parameter originalURL: The unshortened URL.
+	 - Returns: A `ShortenedURL` object that is either based on the `originalURL` (if no shortening is required) or a shortened URL
+	 */
+	public func getShortenedURL(_ originalURL: URL) -> ShortenedURL {
+		return ShortenedURL(url: originalURL, originalName: "TODO", state: .unshortened, nameFileURL: nil)
 	}
 
 	/**
 	 Undos .c9s shortening defined in vault format 7 (if required).
 
-	- Parameter shortenedURL: A potentially shortened URL.
-	- Parameter contentLoader: A closure loading the contents of `nameC9SURL`.
-	- Parameter nameC9SURL: The URL of a `name.c9s` that needs to be loaded.
-	- Returns: Either `shortenedURL` if no shortening was applied or the original ("inflated") URL
-	*/
-	func getOriginalURL(_ shortenedURL: URL, contentLoader: (_ nameC9SURL: URL) -> Promise<Data>) -> Promise<URL> {
+	 - Parameter shortenedURL: A potentially shortened URL.
+	 - Parameter contentLoader: A closure loading the contents of `nameC9SURL`.
+	 - Parameter nameC9SURL: The URL of a `name.c9s` that needs to be loaded.
+	 - Returns: Either `shortenedURL` if no shortening was applied or the original ("inflated") URL
+	 */
+	public func getOriginalURL(_ shortenedURL: URL, contentLoader: (_ nameC9SURL: URL) -> Promise<Data>) -> Promise<URL> {
 		return Promise(shortenedURL)
 	}
 
+	internal func replaceCiphertextFileName(_ url: URL, with replacement: String) -> URL {
+		return url.replacingPathComponent(atIndex: ciphertextNameCompIdx, with: replacement, isDirectory: url.hasDirectoryPath)
+	}
+
+	private func deflateFileName(_ inflatedName: String) -> String {
+		let bytes = [UInt8](inflatedName.precomposedStringWithCanonicalMapping.utf8)
+		var digest = [UInt8](repeating: 0x00, count: Int(CC_SHA1_DIGEST_LENGTH))
+		CC_SHA1(bytes, UInt32(bytes.count) as CC_LONG, &digest)
+		return Data(digest).base64UrlEncodedString()
+	}
 }
--- CloudAccessTests/VaultFormat7/VaultFormat7ShortenedNameCacheTests.swift
+++ CloudAccessTests/VaultFormat7/VaultFormat7ShortenedNameCacheTests.swift
@@ -10,11 +10,36 @@ import XCTest
 @testable import CloudAccess
 
 class VaultFormat7ShortenedNameCacheTests: XCTestCase {
-	let cache = VaultFormat7ShortenedNameCache()
+	func testReplaceCiphertextFileName1() throws {
+		let vaultRoot = URL(fileURLWithPath: "/foo/bar")
+		let cache = VaultFormat7ShortenedNameCache(vaultURL: vaultRoot)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+		let originalURL = URL(fileURLWithPath: "/foo/bar/d/2/30/loooooong.c9r/dir.c9r", isDirectory: true)
+		let shortened = cache.replaceCiphertextFileName(originalURL, with: "short.c9s")
 
+		XCTAssertEqual("/foo/bar/d/2/30/short.c9s/dir.c9r", shortened.path)
+		XCTAssertTrue(shortened.hasDirectoryPath)
+	}
+
+	func testReplaceCiphertextFileName2() throws {
+		let vaultRoot = URL(fileURLWithPath: "/foo")
+		let cache = VaultFormat7ShortenedNameCache(vaultURL: vaultRoot)
+
+		let originalURL = URL(fileURLWithPath: "/foo/d/2/30/loooooong.c9r")
+		let shortened = cache.replaceCiphertextFileName(originalURL, with: "short.c9s")
+
+		XCTAssertEqual("/foo/d/2/30/short.c9s", shortened.path)
+		XCTAssertFalse(shortened.hasDirectoryPath)
+	}
+
+	func testReplaceCiphertextFileName3() throws {
+		let vaultRoot = URL(fileURLWithPath: "/")
+		let cache = VaultFormat7ShortenedNameCache(vaultURL: vaultRoot)
+
+		let originalURL = URL(fileURLWithPath: "/d/2/30/loooooong.c9r/dir.c9r/bullshit")
+		let shortened = cache.replaceCiphertextFileName(originalURL, with: "short.c9s")
+
+		XCTAssertEqual("/d/2/30/short.c9s/dir.c9r/bullshit", shortened.path)
+		XCTAssertFalse(shortened.hasDirectoryPath)
+	}
 }

cryptomator/cloud-access-swift

commit 2562448c5025ad3fa35f8c6de2d6b835a24a0909

Minor optimization during moveItem in Google Drive
[ci skip]

Sources/CryptomatorCloudAccess/GoogleDrive/GoogleDriveCloudProvider.swift
@@ -164,9 +164,7 @@ public class GoogleDriveCloudProvider: CloudProvider {
 	}
 
 	private func moveItem(from sourceCloudPath: CloudPath, to targetCloudPath: CloudPath) -> Promise<Void> {
-		return resolveParentPath(forItemAt: targetCloudPath).then { _ in
-			return self.checkForItemExistence(at: targetCloudPath)
-		}.then { itemExists -> Void in
+		return checkForItemExistence(at: targetCloudPath).then { itemExists -> Void in
 			if itemExists {
 				throw CloudProviderError.itemAlreadyExists
 			}

Sources/CryptomatorCloudAccess/OneDrive/OneDriveCloudProvider.swift
@@ -134,8 +134,8 @@ public class OneDriveCloudProvider: CloudProvider {
 	}
 
 	private func moveItem(from sourceCloudPath: CloudPath, to targetCloudPath: CloudPath) -> Promise<Void> {
-		return checkForItemExistence(at: targetCloudPath).then { targetItemAlreadyExists in
-			if targetItemAlreadyExists {
+		return checkForItemExistence(at: targetCloudPath).then { itemExists in
+			if itemExists {
 				throw CloudProviderError.itemAlreadyExists
 			}
 		}.then { _ -> Promise<(OneDriveItem, OneDriveItem)> in

commit 5a1e3213e52f6df508757ada66c700a3c5bad02c

Fixed regression caused by optimization in 2562448c5025ad3fa35f8c6de2d6b835a24a0909

Sources/CryptomatorCloudAccess/GoogleDrive/GoogleDriveCloudProvider.swift
@@ -465,9 +465,7 @@ public class GoogleDriveCloudProvider: CloudProvider {
 		if onlyItemNameChangedBetween(sourceCloudPath, and: targetCloudPath) {
 			return Promise(query)
 		} else {
-			let sourceParentCloudPath = sourceCloudPath.deletingLastPathComponent()
-			let targetParentCloudPath = targetCloudPath.deletingLastPathComponent()
-			return all(resolvePath(forItemAt: sourceParentCloudPath), resolvePath(forItemAt: targetParentCloudPath)).then { oldParentItem, newParentItem -> GTLRDriveQuery_FilesUpdate in
+			return all(resolveParentPath(forItemAt: sourceCloudPath), resolveParentPath(forItemAt: targetCloudPath)).then { oldParentItem, newParentItem -> GTLRDriveQuery_FilesUpdate in
 				// but resolve the target's parent if it is a shortcut
 				let resolvedNewParentIdentifier = newParentItem.shortcut?.targetIdentifier ?? newParentItem.identifier
 				query.addParents = resolvedNewParentIdentifier
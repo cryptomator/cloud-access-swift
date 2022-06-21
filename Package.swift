9456e9a1881414bc3b6d5f518eb1a6b55793f321
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2022-06-21 11:36:52 +0200

    Fixed swiftformat error

--- Package.swift
+++ Package.swift
@@ -46,8 +46,8 @@ let package = Package(
 		.target(
 			name: "CryptomatorCloudAccessCore",
 			dependencies: [
-                "AWSCore",
-                "AWSS3",
+				"AWSCore",
+				"AWSS3",
 				"CryptomatorCryptoLib",
 				"GoogleAPIClientForREST_Drive",
 				"GRDB",

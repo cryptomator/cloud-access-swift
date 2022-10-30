70195067ff2d2054abeebb93891fdc2678caa554
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2022-10-30 17:35:10 +0100

    Update Dropbox to 7.0.0

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
 		.package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMinor(from: "5.25.0")),
 		.package(url: "https://github.com/openid/AppAuth-iOS.git", .upToNextMinor(from: "1.5.0")),
 		.package(url: "https://github.com/pCloud/pcloud-sdk-swift.git", .upToNextMinor(from: "3.2.0")),
-		.package(url: "https://github.com/phil1995/dropbox-sdk-obj-c-spm.git", .upToNextMinor(from: "6.3.0")),
+		.package(url: "https://github.com/phil1995/dropbox-sdk-obj-c-spm.git", .upToNextMinor(from: "7.0.0")),
 		.package(url: "https://github.com/phil1995/msgraph-sdk-objc-spm.git", .upToNextMinor(from: "1.0.0")),
 		.package(url: "https://github.com/phil1995/msgraph-sdk-objc-models-spm.git", .upToNextMinor(from: "1.3.0"))
 	],

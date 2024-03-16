6b75190dd50ed22fa4f52126d9156d7e5a857ddc
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2024-03-16 13:03:34 +0100

    Actually use the accountId to make Google Drive URLSessions unique

--- Sources/CryptomatorCloudAccess/GoogleDrive/GoogleDriveCloudProvider.swift
+++ Sources/CryptomatorCloudAccess/GoogleDrive/GoogleDriveCloudProvider.swift
@@ -58,7 +58,8 @@ public class GoogleDriveCloudProvider: CloudProvider {
 				configuration.sharedContainerIdentifier = GoogleDriveSetup.constants.sharedContainerIdentifier
 			}
 			let bundleId = Bundle.main.bundleIdentifier ?? ""
-			configuration = URLSessionConfiguration.background(withIdentifier: "Crytomator-GoogleDriveSession-\try (credential.getAccountID())-\(bundleId)")
+            let accountId = try credential.getAccountID()
+			configuration = URLSessionConfiguration.background(withIdentifier: "Crytomator-GoogleDriveSession-\(accountId)-\(bundleId)")
 			configuration.sharedContainerIdentifier = GoogleDriveSetup.constants.sharedContainerIdentifier
 		} else {
 			configuration = URLSessionConfiguration.default

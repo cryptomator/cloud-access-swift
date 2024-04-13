0260c8883d8d10a41fe79598139c786d4263087d
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2024-04-13 16:38:40 +0200

    Migrate LocalFileSystemTests to structured concurrency

---
+++ Sources/CryptomatorCloudAccess/Common/Promise+Async.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Promises
+
+extension Promise {
+    func async() async throws -> Value {
+        try await withCheckedThrowingContinuation { continuation in
+            then { value in
+                continuation.resume(returning: value)
+            }.catch { error in
+                continuation.resume(throwing: error)
+            }
+        }
+    }
+}

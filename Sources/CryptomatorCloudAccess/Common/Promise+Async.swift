f310d2513c6012e30a970e281358168bc8332b75
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2024-04-13 16:40:44 +0200

    Run SwiftFormat

--- Sources/CryptomatorCloudAccess/Common/Promise+Async.swift
+++ Sources/CryptomatorCloudAccess/Common/Promise+Async.swift
@@ -2,13 +2,13 @@ import Foundation
 import Promises
 
 extension Promise {
-    func async() async throws -> Value {
-        try await withCheckedThrowingContinuation { continuation in
-            then { value in
-                continuation.resume(returning: value)
-            }.catch { error in
-                continuation.resume(throwing: error)
-            }
-        }
-    }
+	func async() async throws -> Value {
+		try await withCheckedThrowingContinuation { continuation in
+			then { value in
+				continuation.resume(returning: value)
+			}.catch { error in
+				continuation.resume(throwing: error)
+			}
+		}
+	}
 }
